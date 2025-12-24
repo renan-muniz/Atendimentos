73ef7508b34e42d66f00034c75cc0467c7f9af34
Bisteca <luregat@gmail.com>
2025-12-24 21:19:44 +0100

    .

--- variables.py
+++ variables.py
@@ -1,12 +1,19 @@
 import psycopg2
+from urllib.parse import urlparse
+import os
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-conexion = psycopg2.connect(
-    host = "localhost",
-    database = "controle_atendimentos",
-    user = "postgres",
-    password = "Renan1013",
-    port = 5432
-)
+if DATABASE_URL:
+    conexion = psycopg2.connect(DATABASE_URL)
+
+else:
+    conexion = psycopg2.connect(
+        host = "localhost",
+        database = "controle_atendimentos",
+        user = "postgres",
+        password = "Renan1013",
+        port = 5432,
+    )
 
 
 
